3b9c25cd9f82cc26fa2fd2af1e2e4bd50640ff41
hn-by <hanancherumala2007@gmail.com>
2023-12-29 15:19:31 +0000

    quoteTab

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ This is my personal portfolio
 
 28 december 2023:-
     -Made the helloTab.
+29 december 2023:-
+    -Made the quoteTab.
 
 
--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-const body = document.querySelector('body')
+const body = document.querySelector('body');
+const quoteTab = document.querySelector('.quoteTab');
+const quote = document.querySelector('.quote');
+const author = document.querySelector('.author');
+
+body.addEventListener("click", getQuote)
+
+function getQuote() {
+    fetch("http://quotable.io/random")
+        .then(res => res.json())
+        .then(data => {
+            quote.textContent =  '"' + data.content + '"';
+            author.textContent = '-' + data.author;
+        })
+}
 
 
